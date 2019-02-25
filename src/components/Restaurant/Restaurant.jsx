edeb2e8010935462fcc833d6d49f7291235a810c
nictengkk <nictengkk@gmail.com>
2019-02-25 16:13:45 +0800

    Remove style for card to make width responsive

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function Restaurant({ item }) {
   const { name, openingTime, closingTime, cuisine, imageUrl } = item;
 
-  const cardStyle = { width: "18rem" };
+  // const cardStyle = { width: "18rem" };
 
   return (
     <div className="col-sm-6 col-md-4 col-lg-3 d-flex">
-      <div className="card mb-3" style={cardStyle}>
+      <div className="card mb-3">
         <img className="card-img-top" src={imageUrl} alt={name} />
         <div className="card-body">
           <h5 className="card-title">{name}</h5>

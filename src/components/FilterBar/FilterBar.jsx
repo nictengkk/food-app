970799f9fe9a1b4deedddea6173866b54211160d
nictengkk <nictengkk@gmail.com>
2019-02-26 10:09:39 +0800

    add map to FilterBar Component to render different classes to buttons

--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -1,30 +1,25 @@
 import React from "react";
 
 function FilterBar(props) {
-  const { handleClick } = props;
+  const { handleClick, cuisines } = props;
+
+  getclass = (cuisine, selected) => {
+    return;
+  };
+
   return (
-    <div className="text-center">
+    <div>
       <form className="form-inline mt-3">
         <div className="btn-group ml-3" role="group" aria-label="Basic example">
-          <button
-            type="button"
-            class="btn btn-outline-primary"
-            onClick={() => props.handleClick()}
-          >
-            All
-          </button>
-          <button type="button" class="btn btn-outline-primary">
-            Western
-          </button>
-          <button type="button" class="btn btn-outline-primary">
-            Japanese
-          </button>
-          <button type="button" class="btn btn-outline-primary">
-            Thai
-          </button>
-          <button type="button" class="btn btn-outline-primary">
-            Chinese
-          </button>
+          {cuisines.map(button => (
+            <button
+              type="button"
+              class={getClass}
+              onClick={() => props.handleClick()}
+            >
+              {name}
+            </button>
+          ))}
         </div>
       </form>
     </div>

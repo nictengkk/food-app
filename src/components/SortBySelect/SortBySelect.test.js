39c333bad52fcf6d8a9df9276b85db03e6a1f9fd
nictengkk <nictengkk@gmail.com>
2019-02-27 12:15:06 +0800

    add two tests to SortBySelect

---
+++ src/components/SortBySelect/SortBySelect.test.js
@@ -0,0 +1,34 @@
+import "react-testing-library/cleanup-after-each";
+import "jest-dom/extend-expect";
+import React from "react";
+import { render } from "react-testing-library";
+import SortBySelect from "./SortBySelect";
+
+test("renders a select list from options", () => {
+  const selectOptions = [
+    { name: "option1", value: "option1Value" },
+    { name: "option2", value: "option2Value" }
+  ];
+
+  const { getByText, getBySelectText } = render(
+    <SortBySelect options={selectOptions} />
+  );
+
+  expect(getBySelectText("option1").children.length).toEqual(2);
+  expect(getByText("option1")).toHaveAttribute("value", selectOptions[0].value);
+  expect(getByText("option1")).toBeInTheDocument();
+  expect(getByText("option2")).toBeInTheDocument();
+});
+
+test("check if Restaurant Name is selected as default", () => {
+  const selectOptions = [
+    { name: "option1", value: "option1Value" },
+    { name: "option2", value: "option2Value" }
+  ];
+
+  const { getByText, getBySelectText } = render(
+    <SortBySelect options={selectOptions} />
+  );
+
+  expect(getBySelectText("option1")).toBeVisible();
+});

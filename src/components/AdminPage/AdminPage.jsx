e92b819db848044246ac24f8dc98e52d4606eeae
nictengkk <nictengkk@gmail.com>
2019-02-28 15:56:11 +0800

    create new button in admin page and create new restaurant form component

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,24 +1,39 @@
-import React, {Component} from "react";
-import RestaurantTable from "../RestaurantTable/RestaurantTable"
-import { getRestaurants, deleteRestaurant } from '../../services/restaurantService';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import RestaurantTable from "../RestaurantTable/RestaurantTable";
+import {
+  getRestaurants,
+  deleteRestaurant
+} from "../../services/restaurantService";
+import "bootstrap/dist/css/bootstrap.css";
 
 class AdminPage extends Component {
   state = {
     restaurants: getRestaurants()
-  }
+  };
 
   handleDelete = restaurantId => {
-    deleteRestaurant(restaurantId)
+    deleteRestaurant(restaurantId);
     this.setState({ restaurants: getRestaurants() });
   };
 
-  render(){
-    const {restaurants} = this.state;
+  render() {
+    const { restaurants } = this.state;
     return (
-      <div data-testid="admin-page">
-        <RestaurantTable restaurants={restaurants} handleDelete={this.handleDelete}/>
+      <div>
+        <div className="d-flex justify-content-end mb-2">
+          <Link className="btn btn-primary" to="/restaurants/new">
+            Create New
+          </Link>
+        </div>
+        <div data-testid="admin-page">
+          <RestaurantTable
+            restaurants={restaurants}
+            handleDelete={this.handleDelete}
+          />
+        </div>
       </div>
-    )
+    );
   }
 }
 
---
+++ src/components/RestaurantForm/RestaurantForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class RestaurantForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Restaurant Form</h1>
+      </div>
+    );
+  }
+}
+
+export default RestaurantForm;

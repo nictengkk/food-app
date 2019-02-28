c9c873b7948a6488b0c77b05f04eb0dcbc25f328
nictengkk <nictengkk@gmail.com>
2019-02-28 12:57:48 +0800

    install fontawesome package, add utensil logo to navbrand

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
+        <FontAwesomeIcon className="mr-1" icon="utensils" />
         Food App
       </Link>
       <button
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUtensils } from "@fortawesome/free-solid-svg-icons";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+
+library.add(faUtensils);
 
 ReactDOM.render(
   <BrowserRouter>
